c98bb8976328f679fd64355c3bac9f3e219bea9e
Nate Sawant <natesawant@gmail.com>
2021-05-26 12:34:19 -0400

    Added methods to Material class

--- Material.java
+++ Material.java
@@ -1,5 +1,39 @@
+import java.awt.Color;
 public class Material {
+    private Color matColor = Color.BLACK;
         public static void main(String[] args) {
 
+    }
+
+    public boolean goUp(Material[] adjCells) { 
+        //adjCells[0] = up, 
+        //adjCells[1] = down, 
+        //adjCells[2] = right 
+        //adjCells[3] = left
+        return false;
+    }
+
+    public boolean goDown(Material[] adjCells) { 
+        //adjCells[0] = up, 
+        //adjCells[1] = down, 
+        //adjCells[2] = right 
+        //adjCells[3] = left
+        return false;
+    }
+
+    public boolean goRight(Material[] adjCells) { 
+        //adjCells[0] = up, 
+        //adjCells[1] = down, 
+        //adjCells[2] = right 
+        //adjCells[3] = left
+        return false;
+    }
+    
+    public boolean goLeft(Material[] adjCells) { 
+        //adjCells[0] = up, 
+        //adjCells[1] = down, 
+        //adjCells[2] = right 
+        //adjCells[3] = left
+        return false;
     }
 }
