84cfbff03c35187e76b0c7a06e96819c6dcfc9f4
Nate Sawant <natesawant@gmail.com>
2021-05-28 17:15:32 -0400

    Started new LIBGDX test

---
+++ gdxtestgame1/desktop/src/com/gdxtestgame1/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdxtestgame1.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdxtestgame1.game.gdxtestgame1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new gdxtestgame1(), config);
+	}
+}
